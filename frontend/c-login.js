2fcf1ff13f2363fb400770df7efaf52a58faae15
Anuya-10 <anushkayadav9e.gr1@gmail.com>
2025-10-08 01:19:21 +0530

    profile work

--- frontend/c-login.js
+++ frontend/c-login.js
@@ -19,10 +19,9 @@ document.getElementById("loginForm").addEventListener("submit",async function(e)
     const data = await response.json();
 
     if (response.ok) {
-        localStorage.setItem("token", data.token);
-        localStorage.setItem("username", data.user.username);  
-        localStorage.setItem("name", data.user.name);  
-        localStorage.setItem("email", data.user.email); 
+        localStorage.setItem("citizenToken", data.token); // ✅ clearer distinction
+        localStorage.setItem("user", JSON.stringify(data.user));  // ✅ store full user object
+
         alert("Login successful!");
         window.location.href = "./city-dashboard/city-dashboard.html";
       } else {
